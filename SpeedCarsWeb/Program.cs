04f360cda493327ba06533ce24300083d627607a
selvaparur <mail2meselva@gmail.com>
2020-01-21 13:35:08 +0530

    t
    t

--- SpeedCarsWeb/Program.cs
+++ SpeedCarsWeb/Program.cs
@@ -14,7 +14,7 @@ namespace SpeedCarsWeb
     {
         public static void Main(string[] args)
         {
-            // test test Selva Test 1132020
+            // test test Selva Test 1132020 test
             CreateWebHostBuilder(args).UseKestrel().UseIISIntegration().Build().Run();
         }
 
